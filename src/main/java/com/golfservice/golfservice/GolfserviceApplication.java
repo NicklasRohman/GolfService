dcf5f3f7445f8dee6094325655fa74029ac14eb2
NicklasRohman <NicklasRohman@gmail.com>
2020-01-03 19:41:53 +0100

    all files

---
+++ src/main/java/com/golfservice/golfservice/GolfserviceApplication.java
@@ -0,0 +1,13 @@
+package com.golfservice.golfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/golfservice/golfservice/GolfserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golfservice.golfservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
