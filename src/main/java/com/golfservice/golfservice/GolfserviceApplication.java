d66f559f6b1a5f36b00927db3985b1ea7e1e0cfd
NicklasRohman <NicklasRohman@gmail.com>
2020-01-03 21:01:32 +0100

    comment out

--- src/main/java/com/golfservice/golfservice/GolfserviceApplication.java
+++ src/main/java/com/golfservice/golfservice/GolfserviceApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GolfserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GolfserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(GolfserviceApplication.class, args);
+    }
 
 }
