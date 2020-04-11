b4407f0e4451384c8f28be8d26eee08e3ac3add1
Nicklas Rohman <nicroh@jpj.com>
2020-04-11 12:27:55 +0200

    added service files

--- src/main/java/com/golfservice/golfservice/service/CourseService.java
+++ src/main/java/com/golfservice/golfservice/service/CourseService.java
@@ -1,8 +1,39 @@
 package com.golfservice.golfservice.service;
 
+import com.golfservice.golfservice.dto.CourseDTO;
 import com.golfservice.golfservice.service.interfaces.CourseServiceInterface;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CourseService implements CourseServiceInterface {
+
+
+    List<CourseDTO> listOfCourses;
+
+    private void fakeDB() {
+        listOfCourses = new ArrayList<>();
+        listOfCourses.add(new CourseDTO("Läckö GK", 1));
+        listOfCourses.add(new CourseDTO("Lidköpings GK", 2));
+    }
+
+    @Override
+    public List<CourseDTO> getAllCourses() {
+        fakeDB();
+        return listOfCourses;
+    }
+
+    @Override
+    public CourseDTO getCoursesById(int courseId) {
+        fakeDB();
+        for (CourseDTO co : listOfCourses) {
+            if (co.getCourseId() == courseId) {
+                return co;
+            }
+        }
+
+        return null;
+    }
 }
--- src/main/java/com/golfservice/golfservice/service/interfaces/CourseServiceInterface.java
+++ src/main/java/com/golfservice/golfservice/service/interfaces/CourseServiceInterface.java
@@ -1,4 +1,13 @@
 package com.golfservice.golfservice.service.interfaces;
 
+import com.golfservice.golfservice.dto.CourseDTO;
+
+import java.util.List;
+
 public interface CourseServiceInterface {
+
+    public List<CourseDTO> getAllCourses();
+
+    public CourseDTO getCoursesById(int courseId);
+
 }
